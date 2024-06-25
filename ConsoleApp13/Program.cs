7a4a6f4e1a13f655297e44bb2912829fdff4b90f
caio0102030405 <163909975+caio0102030405@users.noreply.github.com>
2024-06-25 16:38:54 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numero = new int[5];
+int resultado = 0;
+for(int contador = 0; contador <5; contador+=1)
+{
+    Console.WriteLine("diga um numero.");
+    numero[contador] = int.Parse(Console.ReadLine());
+}
+resultado = numero[0] + numero[1] + numero[2] + numero[3] + numero[4];
+
+Console.WriteLine(resultado);
